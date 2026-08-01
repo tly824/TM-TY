173f2d86c13b2ae6c508f11cdadb87fbc6a9f143
tly824 <156475643@qq.com>
2026-08-01 14:50:11 +0800

    Add files via upload

---
+++ drpy_js/宝片视频.js
@@ -0,0 +1,7 @@
+var rule = {
+    模板: '自动',
+    title: '宝片视频',
+    host: 'https://ibaopian.pro',
+    searchUrl: '/vod/search/page/fypage/wd/**.html',
+    url: '/vod/show/id/fyclass/page/fypage.html',
+}
